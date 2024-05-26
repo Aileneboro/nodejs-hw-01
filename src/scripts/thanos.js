1be6fded0c0f96b02f4daea45dcf08d599704768
Aileneboro <a.borovikova89@gmail.com>
2024-05-26 19:52:25 +0300

    7

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    let contacts = JSON.parse(data);
+
+    const filterContacts = contacts.filter(() => Math.random() >= 0.5);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(filterContacts), 'utf8');
+    console.log('Random contacts have been deleted');
+  } catch (error) {
+    console.error('Error', error);
+  }
+};
 
 await thanos();
